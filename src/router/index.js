029a6ebbea5f62644be9be8461113cb93c49d23c
dj-neza <neza.djukic@gmail.com>
2024-10-19 12:14:37 +0200

    Add page

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import HekatonView from '../views/HekatonView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import HekatonView from "../views/HekatonView.vue";
+import AboutUsView from "../views/AboutUsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/hekaton',
-      name: 'hekaton',
-      component: HekatonView
-    }
-  ]
-})
+      path: "/about-us",
+      name: "about-us",
+      component: AboutUsView,
+    },
+    {
+      path: "/hekaton",
+      name: "hekaton",
+      component: HekatonView,
+    },
+  ],
+});
 
-export default router
+export default router;
