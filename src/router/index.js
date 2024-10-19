98b7b1420ecc2ee709320bd98243c21b6b164d52
Patricija Brečko <patricija@danesjenovdan.si>
2024-10-19 20:32:56 +0200

    add events and activities

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,44 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import HekatonView from "../views/HekatonView.vue";
+// import HekatonView from "../views/HekatonView.vue";
 import AboutUsView from "../views/AboutUsView.vue";
+import ActivitiesView from '../views/ActivitiesView.vue';
+import EventsView from '../views/EventsView.vue';
 import PartnershipsView from "../views/PartnershipsView.vue";
-import ActivitiesView from "../views/ActivitiesView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
     },
     {
-      path: "/o-nas",
-      name: "o-nas",
+      path: '/o-nas',
+      name: 'o-nas',
       component: AboutUsView,
     },
     {
-      path: "/partnerstvo",
-      name: "partnerstvo",
+      path: '/partnerstvo',
+      name: 'partnerstvo',
       component: PartnershipsView,
     },
     {
-      path: "/aktivnosti",
-      name: "sktivnosti",
+      path: '/aktivnosti',
+      name: 'aktivnosti',
       component: ActivitiesView,
     },
     {
-      path: "/hekaton",
-      name: "hekaton",
-      component: HekatonView,
+      path: '/dogodki',
+      name: 'dogodki',
+      component: EventsView,
     },
+    // {
+    //   path: '/hekaton',
+    //   name: 'hekaton',
+    //   component: HekatonView,
+    // },
   ],
 });
 
--- src/views/ActivitiesView.vue
+++ src/views/ActivitiesView.vue
@@ -1,265 +1,248 @@
 <template>
-  <main>
-    <section class="section">
-      <h3>Aktivnosti</h3>
-    </section>
-    <section class="section">
-      <div>
-        <h3>Pretekli dogodki</h3>
-        <h4>Sezona 23/24</h4>
-        <div class="list">
-          <div class="whiteSquare">
-            <img src="@/assets/pretekli-dogodki/uspeh.png" alt="Koda za uspeh dogodek" />
-            <div class="column">
-              <strong>Koda za uspeh: delavnica načrtovanja kariere v tehnologiji in voden pogovor s strokovnjakinjami na
-                področju tehnologije
-              </strong>
-              <p>Vodja delavnice: Simona Kek</p>
-              <p>Sodelovale v vodenem pogovoru: Alja Isaković, Biserka Cvetkovska, Tamara Štumperger</p>
-              <p>Pogovor je vodila Eva Vidmar.
-              </p>
-            </div>
-          </div>
-          <div class="whiteSquare">
-            <img src="@/assets/pretekli-dogodki/kripto.jpg" alt="Kripto dogodek" />
-            <div class="column">
-              <strong>Crypto 101
-              </strong>
-              <p>Kako izgleda področje kriptografije in kako se lahko vanj vključimo kot razvijalke.
-              </p>
-              <p class="strong">Govorka: Anja Blaj
-              </p>
-            </div>
-          </div>
-          <div class="whiteSquare">
-            <img src="@/assets/pretekli-dogodki/tehnicni-intervju.jpg" alt="Tehnični intervju dogodek" />
-            <div class="column">
-              <strong>Kako se pripravim na tehnični intervju
-              </strong>
-              <p>Nasveti za pripravo na tehnični intervju in oblikovanje CV-ja ter primeri dobrih praks v obliki
-                simulacije tehničnega intervjuja.
-              </p>
-              <p class="strong">Govorki: Vika Breznik, Leja Jenič
-              </p>
-            </div>
-          </div>
-          <div class="whiteSquare">
-            <img src="@/assets/pretekli-dogodki/startup.jpg" alt="Startup dogodek" />
-            <div class="column">
-              <strong>Startup 101
-              </strong>
-              <p>Kako idejo pripeljemo do stvaritve lastnega podjetja.
-              </p>
-              <p class="strong">Govorka: Špela Rozman
-              </p>
-            </div>
-          </div>
-          <div class="whiteSquare">
-            <img src="@/assets/pretekli-dogodki/hekaton.jpeg" alt="Hekaton dogodek" />
-            <div class="column">
-              <strong>Hekaton z odprtimi podatki za družbeno korist
-              </strong>
-              <p>Dvodnevni dogodek, na katerem je 40 udeleženk in udeležencev programiralo rešitve za družbeno korist s
-                pomočjo odprtih podatkov. Na dogodku je dr. Matevž Pesek predstavil dva produkta, ki snovita na odprtih
-                podatkih: avtolog.si in tocen.si.
-              </p>
-              <p class="strong">Govorec: Dr. Matevž Pesek
-              </p>
-              <p class="strong">V sodelovanju s podjetji Danes je nov dan, Outbrain, Celtra, Outfit7, Sportradar,
-                Infincube in tretton37
-              </p>
-            </div>
-          </div>
-          <div class="whiteSquare">
-            <img src="@/assets/pretekli-dogodki/after-class.jpg" alt="After class dogodek" />
-            <div class="column">
-              <strong>After Class: Ready, Set, Succeed
-              </strong>
-              <p>Nasveti za organizacijo časa in ustvarjanje strategije za uspešno študijsko leto.
-              </p>
-              <p class="strong">Govorki: Simona Kek, Patricija Brečko
-              </p>
-            </div>
-          </div>
-        </div>
-        <h4>Sezona 22/23</h4>
-        <div class="list">
-          <div class="whiteSquare">
-            <img src="@/assets/pretekli-dogodki/ux-ui.jpg" alt="Ux/ui dogodek" />
-            <div class="column">
-              <strong>Kako postanem UX/UI oblikovalka in delo v tujini
-              </strong>
-              <p>Zgodbe Maše in Dragane, kako sta postali UX/UI razvijalki, kakšne so njune izkušnje z delom v tujini in
-                kaj bi svetovale tistim, ki si želijo po njunih stopinjah.
-              </p>
-              <p class="strong">Govorki: Maša Planinc, Dragana Đekić</p>
-            </div>
-          </div>
-          <div class="whiteSquare">
-            <img src="@/assets/pretekli-dogodki/podatkovna-znanstvenica.jpg" alt="podatkovna znanstvenica dogodek" />
-            <div class="column">
-              <strong>Kako postanem podatkovna znanstvenica in kako pripravim zmagovalni pitch
-              </strong>
-              <p>Zgodba Grete, kako je postala podatkovna znanstvenica, kaj sploh je podatkovna znanost in kako se
-                razlikuje od ostalih vlog na področju dela s podatki ter nasveti za pripravo zmagovalnega pitcha.
-              </p>
-              <p class="strong">Govorki: Greta Gašparac, Eva Vidmar</p>
-            </div>
-          </div>
-          <div class="whiteSquare">
-            <div class="column">
-              <strong>Kako postanem FullStack razvijalka in priprava na prvo zaposlitev
-              </strong>
-              <p>Zgodba Patricije, kako je postala FullStack razvijalka in kako lahko to postanejo tudi drugi ter
-                nasveti za pripravo na prvo zaposlitev.
-              </p>
-              <p class="strong">Govorki: Patricija Brečko, Simona Kek</p>
-            </div>
-          </div>
-          <div class="whiteSquare">
-            <div class="column">
-              <strong>Kako postanem BackEnd razvijalka in umetnost pogajanja
-              </strong>
-              <p>Zgodba Katarine, kako je postala vlogi BackEnd razvijalka in kaj bi svetovala tistim, ki si to želijo
-                postati. Luka je podal nasvete za uspešno pogajanje v službi in življenju.</p>
-              <p class="strong">Govorca: Katarina Stupica, Luka Androjna
-              </p>
-            </div>
-          </div>
-          <div class="whiteSquare">
-            <img src="@/assets/pretekli-dogodki/fe-tujina.jpg" alt="fe tujina dogodek" />
-            <div class="column">
-              <strong>Kako postanem FrontEnd razvijalka in kako začnem kariero v tujini
-              </strong>
-              <p>Zgodba Neže, kako je postala FrontEnd razvijalka in kako je začela kariero v tujini.
-              </p>
-              <p class="strong">Govorka: Neža Đukić
+    <main>
+        <section class="title">
+            <h1>Aktivnosti</h1>
+        </section>
 
-              </p>
+        <section class="row">
+            <div class="card">
+                <img src="@/assets/activities/mesecni-dogodki.jpg" alt="ZAKLJUČNI DOGODEK SEZONE">
+                <div class="card-body">
+                    <h3>Mesečni dogodki</h3>
+                    <p>Na mesečnih srečanjih obravnavamo različne karierne in tehnične teme, govorimo o mehkih veščinah,
+                        gostimo strokovnjakinje iz industrije ter se družimo in izmenjujemo ideje v sproščenem okolju ob
+                        prigrizkih
+                        in pijači.</p>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
+
+            <div class="card">
+                <img src="@/assets/activities/coffee&&code.png" alt="ZAKLJUČNI DOGODEK SEZONE">
+                <div class="card-body">
+                    <h3>Coffee&&Code</h3>
+                    <p>Coffee&&Code je serija neformalnih druženj ob kavi, kjer skupaj programiramo na projektih, ki si
+                        jih vsaka
+                        izbere sama. Lahko prineseš svojo domačo nalogo, osebni projekt, tutorial ali karkoli drugega,
+                        na čemer
+                        želiš delati. Poleg programiranja je to tudi priložnost za sproščeno debato, izmenjavo idej in
+                        iskanje
+                        motivacije za začetek ali nadaljevanje projekta.</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="@/assets/activities/discord.png" alt="ZAKLJUČNI DOGODEK SEZONE">
+                <div class="card-body">
+                    <h3>Discord skupnost</h3>
+                    <p>Discord skupnost je varen prostor za komunikacijo, kjer izmenjujemo ideje, razpravljamo o
+                        zanimivih temah,
+                        delimo dogodke, uporabne vire in priložnosti za osebni ter karierni razvoj.</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="@/assets/activities/bookclub.jpg" alt="ZAKLJUČNI DOGODEK SEZONE">
+                <div class="card-body">
+                    <h3>Knjižni klub</h3>
+                    <p>Knjižni klub je prostor za vse, ki poleg tehnologije uživamo tudi v branju in razpravah. Vsakih
+                        nekaj
+                        mesecev izberemo knjige, vezane na teme, kot so tehnologija, vloga žensk v družbi, osebnostna
+                        rast in razvoj
+                        kariere. Knjige, ki jih izbiramo, pogosto naslavljajo izzive, s katerimi se srečujemo kot ženske
+                        v
+                        tehnoloških poklicih, ali pa nas navdihujejo z zgodbami o moči, uspehu in spremembah.</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="@/assets/activities/novicnik.png" alt="ZAKLJUČNI DOGODEK SEZONE">
+                <div class="card-body">
+                    <h3>Novičnik</h3>
+                    <p>Preko novičnika delimo aktualne informacije o prihajajočih dogodkih in naših drugih aktivnostih.
+                        Prav tako
+                        obveščamo o drugih zanimivih dogodkih in novostih iz sveta tehnologije.</p>
+                </div>
+            </div>
+        </section>
+
+    </main>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            "menuOpen": false
+        }
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 section {
-  padding-left: 32px;
-  padding-right: 32px;
-  max-width: 1160px;
-  margin: auto;
+    max-width: 1160px;
+    margin: 60px auto;
+
+    p {
+        font-size: 20px;
+        margin-bottom: 20px;
+
+        strong {
+            font-weight: 700;
+        }
+
+        @media (min-width: 992px) {
+            font-size: 28px;
+        }
+    }
 }
 
-strong {
-  font-weight: 700;
-  color: var(--color-red);
+// hr {
+//   border: none;
+//   border-bottom: 2px solid var(--color-red);
+// }
+
+.title {
+    h1 {
+        color: var(--color-red);
+        text-align: center;
+        font-weight: 700;
+        font-size: 40px;
+
+        @media (min-width: 992px) {
+            font-size: 48px;
+        }
+    }
+
+    h2 {
+        color: var(--color-red);
+        // text-align: center;
+        font-weight: 700;
+        font-size: 36px;
+        margin-bottom: 30px;
+    }
 }
 
-.strong {
-  font-weight: 600;
+.next-event {
+    display: flex;
+
+    >div:first-child {
+        img {
+            max-width: 500px;
+        }
+    }
+
+    >div:last-child {
+        margin-left: 40px;
+    }
+
+    p.small {
+        margin-bottom: 10px;
+        font-size: 24px;
+        font-weight: 700;
+    }
+
+    h2 {
+        font-weight: 700;
+        color: var(--color-red);
+        margin-bottom: 10px;
+    }
 }
 
-.fullWidthSection {
-  width: 100%;
-  max-width: unset;
-  background-color: var(--color-light);
+.row {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 24px;
+    flex-wrap: wrap;
+    margin-top: 32px;
+    margin-bottom: 32px;
 
-  li {
-    color: var(--color-dark);
-  }
+    // @media (min-width: 944px) {
+    //   flex-wrap: nowrap;
+    // }
 }
 
-.list {
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  gap: 16px;
-}
-
-.fullWidthSection>div {
-  padding-left: 32px;
-  padding-right: 32px;
-  max-width: 1160px;
-  margin: auto;
-}
-
-h4 {
-  color: var(--color-red);
-  font-size: 32px;
-  margin-bottom: 16px;
-}
-
-.column {
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-}
-
-.whiteSquare {
-  background-color: var(--color-light);
-  border: 2px solid var(--color-dark);
-  border-radius: 8px;
-  color: var(--color-dark);
-  padding: 32px;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  align-self: stretch;
-
-  @media (min-width: 600px) {
-    flex-direction: row;
-  }
-
-  h4 {
-    color: var(--color-dark);
-    font-weight: 600;
-    font-size: 24px;
-  }
-
-  p {
-    font-size: 16px;
-  }
-
-  img {
-    max-height: 200px;
-    width: auto;
-    max-width: 270px;
+.card {
+    width: 30%;
+    background-color: var(--color-light);
     border: 2px solid var(--color-dark);
     border-radius: 8px;
-  }
+    color: var(--color-dark);
+    padding: 32px;
+    // text-align: center;
+    // justify-content: center;
+    // display: flex;
+    // flex-direction: column;
+    // gap: 16px;
+    align-self: stretch;
+
+    h3 {
+        color: var(--color-dark);
+        font-weight: 600;
+        font-size: 24px;
+        margin-top: 20px;
+        margin-bottom: 10px;
+    }
+
+    img {
+        width: 100%;
+        max-height: 250px;
+        object-fit: cover;
+        object-position: center;
+    }
+
+    p {
+        font-size: 20px;
+    }
 }
 
-.section {
-  a {
-    color: var(--color-red);
-    font-weight: 600;
-
-    &:hover {
-      color: var(--color-red);
-    }
-  }
-
-  padding-top: 60px;
-  padding-bottom: 60px;
-
-  h3 {
-    color: var(--color-red);
-    font-size: 32px;
-    line-height: 1;
-    font-weight: 600;
-    text-align: center;
-    margin-bottom: 40px;
+footer {
+    padding-top: 80px;
+    padding-bottom: 40px;
 
     @media (min-width: 992px) {
-      font-size: 48px;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        column-gap: 80px;
     }
-  }
-}
 
-.centeredText {
-  text-align: center;
+    .newsletter {
+        p {
+            margin-bottom: 60px;
+        }
+    }
+
+    .instagram {
+        display: flex;
+        flex-direction: column;
+        justify-content: end;
+        text-align: center;
+        margin-top: 40px;
+
+        @media (min-width: 992px) {
+            text-align: end;
+            margin-top: 0;
+        }
+
+        a {
+            display: inline-block;
+            margin-bottom: 20px;
+
+            img {
+                width: 40px;
+            }
+        }
+
+        p {
+            font-size: 18px;
+            margin-bottom: 20px;
+
+            a {
+                color: var(--color-light);
+                font-weight: 600;
+            }
+        }
+    }
 }
 </style>
