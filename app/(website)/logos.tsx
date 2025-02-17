ce42dbbf583935909e15009865ad15efceb9f91f
Kaja Skerlj <kaja.skerlj@gmail.com>
2025-02-17 16:50:34 +0100

    Added logos of new partners

--- app/(website)/logos.tsx
+++ app/(website)/logos.tsx
@@ -6,18 +6,30 @@ export function Logos() {
   const logos = [
     {
       id: "logo-1",
-      src: "/assets/celtra-logo.webp",
-      href: "https://celtra.com/",
-      alt: "Celtra logo",
-    },
-    {
-      id: "logo-2",
       src: "/assets/impact-hub-logo.webp",
       href: "https://ljubljana.impacthub.net/",
       alt: "Impact Hub Ljubljana logo",
     },
     {
+      id: "logo-2",
+      src: "/assets/celtra-logo.webp",
+      href: "https://celtra.com/",
+      alt: "Celtra logo",
+    },
+    {
       id: "logo-3",
+      src: "/assets/teads-logo.webp",
+      href: "https://www.teads.com/",
+      alt: "Teads logo",
+    },
+    {
+      id: "logo-4",
+      src: "/assets/dewesoft-logo.svg",
+      href: "https://dewesoft.com/",
+      alt: "Dewesoft logo",
+    },
+    {
+      id: "logo-5",
       src: "/assets/fri-logo.webp",
       href: "https://fri.uni-lj.si/sl",
       alt: "FRI logo",
@@ -28,7 +40,7 @@ export function Logos() {
     <PageWrapper>
       <div className="flex flex-col items-center text-center gap-6 md:gap-12">
         <Heading size="xs">{"Podporniki & Partnerji"}</Heading>
-        <div className="flex justify-between items-center gap-8 md:gap-28">
+        <div className="grid grid-cols-3 lg:grid-cols-5 justify-between text-center items-center gap-8 lg:gap-20">
           {logos.map((logo) => (
             <a
               href={logo.href}
@@ -41,7 +53,7 @@ export function Logos() {
                 height={56}
                 src={logo.src}
                 alt={logo.alt}
-                className="h-14 w-full object-contain grayscale hover:grayscale-0 flex-shrink-0"
+                className="h-14 w-44 object-contain grayscale hover:grayscale-0 flex-shrink-0 hover:scale-105 duration-200"
               />
             </a>
           ))}
--- app/(website)/partnerstvo/partners-list.tsx
+++ app/(website)/partnerstvo/partners-list.tsx
@@ -11,43 +11,52 @@ export function PartnersList() {
         <div className="flex flex-col gap-5 md:gap-10 items-center justify-center">
           <Paragraph size="xl">Zlati partnerji</Paragraph>
           <Image
-            src="/assets/outbrain-logo.webp"
+            src="/assets/teads-logo.webp"
             alt="Outbrain logo"
             width={200}
             height={50}
-            className="h-9 md:h-12 w-full object-contain"
+            className="h-16 w-40 object-contain"
           />
         </div>
 
         {/* Silver Partners */}
         <div className="flex flex-col gap-5 md:gap-10 items-center justify-center">
           <Paragraph size="xl">Srebrni partnerji</Paragraph>
-          <Image
-            src="/assets/celtra-logo.webp"
-            alt="Celtra logo"
-            width={200}
-            height={80}
-            className="h-16 w-full object-contain"
-          />
+          <div className="flex md:flex-row flex-col gap-8 md:gap-16 items-center justify-center">
+            <Image
+              src="/assets/celtra-logo.webp"
+              alt="Celtra logo"
+              width={200}
+              height={80}
+              className="h-16 w-40 object-contain"
+            />
+            <Image
+              src="/assets/dewesoft-logo.svg"
+              alt="Celtra logo"
+              width={200}
+              height={80}
+              className="h-16 w-46 object-contain"
+            />
+          </div>
         </div>
 
         {/* Project Partners */}
         <div className="flex flex-col gap-5 md:gap-10 items-center justify-center">
           <Paragraph size="xl">Projektni partnerji</Paragraph>
-          <div className="flex md:flex-row flex-col gap-8 md:gap-16">
+          <div className="flex md:flex-row flex-col gap-8 md:gap-16 items-center justify-center">
             <Image
               src="/assets/impact-hub-logo.webp"
               alt="Impact Hub Ljubljana logo"
               width={200}
               height={50}
-              className="h-16 w-full object-contain"
+              className="h-16 w-40 object-contain"
             />
             <Image
               src="/assets/fri-logo.webp"
               alt="FRI logo"
               width={220}
               height={50}
-              className="h-16 w-full object-contain"
+              className="h-16 w-40 object-contain"
             />
           </div>
         </div>
