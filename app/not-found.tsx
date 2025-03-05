b12e4dcdbb83853f5ba58692799a19d679bbb08d
Kaja Skerlj <kaja.skerlj@gmail.com>
2025-03-05 15:23:03 +0100

    Adjuted the illustration

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import { PageWrapper } from "./components/page-wrapper";
-import { Heading } from "./components/heading";
 import { LinkButton } from "./components/link-button";
 import { Navbar } from "./components/navbar";
 import { Footer } from "./components/footer";
+import { Paragraph } from "./components/paragraph";
 
 export default function NotFound() {
   return (
     <>
       <Navbar />
       <PageWrapper>
-        <div className="flex flex-col w-full h-[calc(100vh-200px)] items-center justify-center gap-8">
+        <div className="flex flex-col w-full h-[calc(100vh-200px)] items-center justify-center gap-6 md:gap-10">
           <Image
             src="/assets/404.svg"
             width={457}
@@ -18,9 +18,9 @@ export default function NotFound() {
             alt="Not found illustration"
             className="w-full max-w-[457px]"
           />
-          <Heading className="text-center">
+          <Paragraph size="xl" weight="bold" className="text-center">
             {"O ne! Stran ni bila najdena."}
-          </Heading>
+          </Paragraph>
           <LinkButton href="/">{"Nazaj na glavno stran"}</LinkButton>
         </div>
       </PageWrapper>
