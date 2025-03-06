a42866c392aa286bbe921deb3266cddbdba39965
dj-neza <neza.djukic@gmail.com>
2025-03-07 00:03:36 +0100

    add more options to the inline error

--- app/components/inline-error.tsx
+++ app/components/inline-error.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import { Paragraph } from "./paragraph";
+import clsx from "clsx";
 
 type InloneErrorProps = {
   label?: string;
+  hasBox?: boolean;
 };
-export function InlineError({ label }: InloneErrorProps) {
+export function InlineError({ label, hasBox = false }: InloneErrorProps) {
   return (
-    <div className="w-fit p-4 md:p-6 bg-white rounded-2xl border border-black flex gap-5 flex-col items-center text-center">
+    <div
+      className={clsx(
+        "w-fit p-4 md:p-6 rounded-2xl flex gap-5 flex-col items-center text-center",
+        hasBox ? "border border-black bg-white" : ""
+      )}
+    >
       <Image
         src="/assets/error.svg"
         width={176}
