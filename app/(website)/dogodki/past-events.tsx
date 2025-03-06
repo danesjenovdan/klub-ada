9cc82830d35809a347c9c9de7f590c43122bf482
dj-neza <neza.djukic@gmail.com>
2025-03-07 00:54:24 +0100

    Add states to other events

--- app/(website)/dogodki/past-events.tsx
+++ app/(website)/dogodki/past-events.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 import EventComponent from "@/app/components/event-component";
 import { Heading } from "@/app/components/heading";
 import { PageWrapper } from "@/app/components/page-wrapper";
 import { client } from "@/sanity/lib/client";
 import { Event } from "../../utils/interface";
+import { useSanityData } from "@/app/utils/use-sanity-data";
+import { InlineError } from "@/app/components/inline-error";
+import { Suspense } from "react";
+import EventSkeleton from "@/app/components/event-skeleton";
+import { Paragraph } from "@/app/components/paragraph";
+
+const GET_PAST_EVENTS_QUERY = `*[
+  _type == "event" && eventTime <= $today
+] | order(eventTime desc) {
+title,
+description,
+eventImage,
+slug,
+eventTime,
+_id,
+}`;
 
 export async function getEvents(today: string) {
   const query = `*[
@@ -19,10 +37,38 @@ export async function getEvents(today: string) {
   return data;
 }
 
-export default async function PastEvents() {
+function PastEventsContent() {
   const today = new Date().toISOString().split("T")[0];
-  const events: Event[] = await getEvents(today);
+  const { data, error, isLoading } = useSanityData({
+    query: GET_PAST_EVENTS_QUERY,
+    params: { today },
+  });
 
+  if (isLoading) {
+    return [0, 1, 2, 3, 4, 5].map((element) => <EventSkeleton key={element} />);
+  }
+
+  if (error) {
+    return (
+      <div className="flex w-full justify-center">
+        <InlineError />
+      </div>
+    );
+  }
+
+  const events = (data || []) as Event[];
+
+  if (!events.length) {
+    return <Paragraph>Na žalost ne najdemo nobenih dogodkov.</Paragraph>;
+  }
+
+  return events.map((event) => (
+    <div key={event._id} className="w-full">
+      <EventComponent event={event} />
+    </div>
+  ));
+}
+export default function PastEvents() {
   return (
     <PageWrapper>
       <div className="flex flex-col gap-8 md:gap-16">
@@ -31,11 +77,7 @@ export default async function PastEvents() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {events.map((event) => (
-            <div key={event._id} className="w-full">
-              <EventComponent event={event} />
-            </div>
-          ))}
+          <PastEventsContent />
         </div>
       </div>
     </PageWrapper>
