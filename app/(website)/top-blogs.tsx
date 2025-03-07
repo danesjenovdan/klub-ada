4c63559cbd575213b8bd71fa975bf39eb8109c79
dj-neza <neza.djukic@gmail.com>
2025-03-07 16:21:36 +0100

    Fix up the top blogs

--- app/(website)/top-blogs.tsx
+++ app/(website)/top-blogs.tsx
@@ -1,9 +1,25 @@
+"use client";
+
 import { Heading } from "../components/heading";
 import { LinkButton } from "../components/link-button";
 import { PageWrapper } from "../components/page-wrapper";
 import PostComponent from "../components/post-component";
 import { Post } from "../utils/interface";
 import { client } from "@/sanity/lib/client";
+import { useSanityData } from "../utils/use-sanity-data";
+import { InlineError } from "../components/inline-error";
+import EventSkeleton from "../components/event-skeleton";
+
+const PINNED_POSTS_QUERY = `*[_type == "post" && pinned] {
+  title,
+  slug,
+  mainImage,
+  categories[]-> {
+    _id,
+    slug,
+    title,
+  }
+}`;
 
 export async function getPinnedPosts() {
   const query = `*[_type == "post" && pinned] {
@@ -20,8 +36,12 @@ export async function getPinnedPosts() {
   return data;
 }
 
-export async function TopBlogs() {
-  const pinnedPosts: Post[] = await getPinnedPosts();
+export function TopBlogs() {
+  const { data, error, isLoading } = useSanityData({
+    query: PINNED_POSTS_QUERY,
+  });
+
+  const pinnedPosts = (data || []) as Post[];
 
   return (
     <PageWrapper bgColor="bg-pink">
@@ -42,13 +62,29 @@ export async function TopBlogs() {
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row md:flex-nowrap gap-4">
-          {pinnedPosts.map((post) => (
-            <div key={post.slug.current} className="basis-1/3">
-              <PostComponent post={post} />
-            </div>
-          ))}
-        </div>
+        {error ? (
+          <div className="w-full flex justify-center">
+            <InlineError hasBox />
+          </div>
+        ) : (
+          <>
+            {isLoading ? (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <EventSkeleton />
+                <EventSkeleton />
+                <EventSkeleton />
+              </div>
+            ) : (
+              <div className="flex flex-col md:flex-row md:flex-nowrap gap-4">
+                {pinnedPosts.map((post) => (
+                  <div key={post.slug.current} className="basis-1/3">
+                    <PostComponent post={post} />
+                  </div>
+                ))}
+              </div>
+            )}
+          </>
+        )}
       </div>
     </PageWrapper>
   );
