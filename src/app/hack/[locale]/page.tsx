432128385f94bfc33b18d6df769b59a98ed4311c
dj-neza <neza.djukic@gmail.com>
2025-08-03 18:51:51 +0200

    Add base for the hero for hack page

--- src/app/hack/[locale]/page.tsx
+++ src/app/hack/[locale]/page.tsx
@@ -1,9 +1,11 @@
 import { useTranslations } from "next-intl";
+import { Hero } from "./hero";
 
 export default function Page() {
   const t = useTranslations("Activities");
   return (
     <>
+      <Hero />
       <div>{t("description")}</div>
     </>
   );
