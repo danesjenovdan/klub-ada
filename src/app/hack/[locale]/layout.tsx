bfaf6fecc8d0449c2def43e6c05e084318bd56e0
dj-neza <neza.djukic@gmail.com>
2025-08-11 15:47:46 +0200

    Fix type

--- src/app/hack/[locale]/layout.tsx
+++ src/app/hack/[locale]/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }>) {
   // Ensure that the incoming `locale` is valid
   const { locale } = await params;
