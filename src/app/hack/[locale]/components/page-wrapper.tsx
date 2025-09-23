1e419a89ee9474b034d1b96837e1f81bcabdfce2
dj-neza <neza.djukic@gmail.com>
2025-09-23 18:54:00 +0200

    Make the animation everywhere

--- src/app/hack/[locale]/components/page-wrapper.tsx
+++ src/app/hack/[locale]/components/page-wrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function PageWrapper({ children }: { children: ReactNode }) {
   return (
-    <section className="px-5 md:px-20 py-10 bg-[#000] md:py-20 box-border max-w-full">
+    <section className="px-5 md:px-20 py-10 md:py-20 box-border max-w-full">
       {children}
     </section>
   );
